be25e5d6a41eed0ad1c99c8f83d2e0ec8c6f9fe4
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-12-18 11:58:02 +0000

    first commit

---
+++ app/src/main/java/ie/wit/walkabout/Walk.kt
@@ -0,0 +1,11 @@
+package ie.wit.walkabout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Walk : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walk)
+    }
+}
